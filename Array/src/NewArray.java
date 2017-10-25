393384e12f39250182ca2072ec26ead2a9e8033a
Ryan LaBarge <ryanclabarge@gmail.com>
2017-10-24 21:46:34 -0400

    Edited some files

--- Array/src/NewArray.java
+++ Array/src/NewArray.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class NewArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] array;
         array = new int[6]; //or you could do the next line
@@ -10,16 +10,29 @@ public class NewArray {
         array[5] = 35;  //or the next two lines
         int n = 4;
         array[n] = 25;
-        System.out.println(array[5]+ " " + array[n]);
-        int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+        System.out.println(array[5] + " " + array[n]);
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         System.out.println("Enter a date in the form MM/DD/YYYY");
         String date = scan.next();
         String[] dates = date.split("/");
         int years = Integer.parseInt(dates[2]);
-        int month = Integer.parseInt(dates[1]);
-        int day = Integer.parseInt(dates[0]);
-        if(years%4==0 && !(years%100==0 && years%400==0)) months[1]=29;
-        System.out.println(months[1]);
+        int month = Integer.parseInt(dates[0]);
+        int day = Integer.parseInt(dates[1]);
+        if (years % 4 == 0) {
+            months[1] = 29;
+            if (years % 100 == 0) {
+                months[1] = 28;
+                if (years % 400 == 0) {
+                    months[1] = 29;
+                }
+            }
+        }
+        if (0<=month && month<=11 && day<=months[month - 1]) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not Valid");
+        }
     }
-
 }
+
+
